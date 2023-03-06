b9e46ab33525a95f113b78dd197135614b8c3480
Alexandre Vieira <avsilva@credipronto.com.br>
2023-03-06 18:13:18 -0300

    chore: remove call to use case read link

--- src/modules/search/SearchController.py
+++ src/modules/search/SearchController.py
@@ -1,15 +1,12 @@
 from src.modules.search.useCase.LoadAllLinksUseCase import LoadAllLinksUseCase
-from src.modules.search.useCase.ReadLinksUseCase import ReadLinksUseCase
 
 
 class SearchController():
 
     def execute(self):
         links = LoadAllLinksUseCase()
-        # read = ReadLinksUseCase()
 
         links.execute()
-        # read.execute()
 
 
     
